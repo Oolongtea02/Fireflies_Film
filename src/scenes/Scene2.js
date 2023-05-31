f732c34ab3fb414813a514a1fe4e94267944d13e
Samuel Zhang <sashzhan@ucsc.edu>
2023-05-30 17:35:25 -0700

    Added firefly object

--- src/scenes/Scene2.js
+++ src/scenes/Scene2.js
@@ -5,11 +5,13 @@ class Scene2 extends Phaser.Scene {
 
     preload() {
         this.load.image('fireflies', './assets/fireflies_bg.png');
+        this.load.image('firefly', './assets/firefly.png');
         this.load.audio('grave', './assets/music/fireflies_bgm.mp3');
     }
 
     create() {
         this.add.image(0, 0, 'fireflies').setOrigin(0, 0);
+        this.sound.play('grave', {loop: false, volume: 0.1});
 
     }
 
