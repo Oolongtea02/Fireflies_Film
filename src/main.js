a86f0a0e030625df6ee92da1786952b7664b4ce1
Samuel Zhang <sashzhan@ucsc.edu>
2023-06-02 21:50:48 -0700

    added particle

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-let config = ({
+/* let config = ({
     type: Phaser.CANVAS,
     width: 680,
     height: 750,
@@ -12,7 +12,20 @@ let config = ({
             debug: true
         }
     }
-}) 
+}) */
+
+let config = {
+    type: Phaser.CANVAS,
+    width: 1056, 
+    height: 594,
+    scene: [ Scene2 ]
+}
 
 let game = new Phaser.Game(config);
 
+// set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
+//set mouse control
+let mouse;
--- src/scenes/Scene2.js
+++ src/scenes/Scene2.js
@@ -1,4 +1,7 @@
 class Scene2 extends Phaser.Scene {
+    /**  @type {Phaser.Physics.Arcade.Sprite} */
+    firefly
+
     constructor() {
         super('Scene2');
     }
@@ -6,16 +9,33 @@ class Scene2 extends Phaser.Scene {
     preload() {
         this.load.image('fireflies', './assets/fireflies_bg.png');
         this.load.image('firefly', './assets/firefly.png');
+        this.load.image('flares', './assets/particles/flares.png');
         this.load.audio('grave', './assets/music/fireflies_bgm.mp3');
     }
 
     create() {
         this.add.image(0, 0, 'fireflies').setOrigin(0, 0);
+        this.add.image(0, 0, 'firefly').setOrigin(0, 0);
         this.sound.play('grave', {loop: false, volume: 0.1});
+
+        mouse = this.input;
+
 
     }
 
     update() {
 
+    }
+
+    fireflyCaught(firefly) {
+        this.emitter = this.add.particles(400, 250, 'flares', {
+            frame: [ 'red', 'yellow', 'green' ],
+            lifespan: 1000,
+            speed: { min: 150, max: 250 },
+            scale: { start: 0.8, end: 0 },
+            gravityY: 150,
+            blendMode: 'ADD',
+            emitting: true
+        });
     }
 }
