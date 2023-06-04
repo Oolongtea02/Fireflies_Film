ece22de67accf3988b3859c02f93dce9ae774d13
tristynl <tristyn-lai@outlook.com>
2023-06-04 15:11:27 -0700

    Added Firefly Prefab

---
+++ src/prefabs/Firefly.js
@@ -0,0 +1,15 @@
+class Firefly extends Phaser.Physics.Arcade.Sprite
+{
+    /**
+	 * @param {Phaser.Scene} scene 
+	 * @param {number} x 
+	 * @param {number} y 
+	 * @param {string} texture 
+	 */
+    constructor(scene, x, y, texture = 'firefly')
+    {
+        super(scene, x, y, texture)
+
+        this.setScale(0.5)
+    }
+}
