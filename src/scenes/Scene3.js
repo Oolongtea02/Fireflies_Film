4335c756f691bfd44b349ac5d61d93f5d598f0e4
tristynl <tristyn-lai@outlook.com>
2023-06-08 19:42:47 -0700

    Added Cooking Pot + Start of Scene 3

--- src/scenes/Scene3.js
+++ src/scenes/Scene3.js
@@ -3,6 +3,7 @@ class Scene3 extends Phaser.Scene {
         super("Scene3");
     }
 
+    count = 0;
       //xCoord = 0;
     preload() {
     this.load.image('background3', './assets/scene3background.jpeg');
@@ -18,17 +19,31 @@ class Scene3 extends Phaser.Scene {
 
         this.add.image(0, 0, 'background3').setOrigin(0,0);
 
-        const cP = new Food(400, 300, 'pot', this, () => console.log("Girl is pressed"));
+        const cP = new Food(400, 300, 'pot', this, () => count += 1);
+
+        let subTextConfig = {
+            fontFamily: 'Georgia',
+            fontSize: '28px',
+            color: 'white', //#843605
+            align: 'center',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+          }
+
+        this.add.text(400, 100, 'Tap Cooking Pot To Start', subTextConfig).setOrigin(0.5);
+
 
         /*const cP = new Food(630, 400, 'cookingPot', this, () => console.log("Girl is pressed"));
         const cN = new Food(630, 300, 'corn', this, () => console.log("Girl is pressed"));
         const gO= new Food(630, 500, 'green', this, () => console.log("Girl is pressed"));
         const bL = new Food(630, 610, 'bowl', this, () => console.log("Girl is pressed"));*/
+    }
 
-
-
-     
-
+    update(){
+        
     }
 
    
