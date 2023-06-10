24ea3226f3a73fe619d400e51b48b39b40aa0640
Samuel Zhang <sashzhan@ucsc.edu>
2023-06-09 20:24:20 -0700

    added sfx and assets

--- src/scenes/Scene3.js
+++ src/scenes/Scene3.js
@@ -7,24 +7,41 @@ class Scene3 extends Phaser.Scene {
       //xCoord = 0;
     preload() {
     this.load.image('background3', './assets/scene3background.jpeg');
-    //this.load.image('corn', './assets/corn.jpeg');
-    //this.load.image('cookingPot', 'pot.png');
-    //this.load.image('bowl', 'rice_bowl.jpeg');
-    //this.load.image('green', 'greenonion.png');
+    this.load.image('corn', './assets/corn.png');
+    this.load.image('cookingPot', './assets/ricePot.png');
+    this.load.image('bowl', './assets/rice_bowl.png');
+    this.load.image('green', './assets/greenonion.png');
+    this.load.audio('cook', './assets/music/cook.wav');
+    this.load.audio('end', './assets/music/ending_bgm.mp3');
     
     }
 
     create() {
-        //this.music = this.sound.play('happyAudio', {loop: false, volume: 0.1});
+        this.music = this.sound.play('end', {loop: false, volume: 0.1});
 
         this.add.image(0, 0, 'background3').setOrigin(0,0);
 
-        const cP = new Food(400, 300, 'pot', this, () => console.log("Girl is pressed"));
+        //const P = new Food(400, 300, 'pot', this, () => console.log("Girl is pressed"));
+        const cP = new Food(400, 300, 'cookingPot', this, () => this.count += 1);
 
-        const cP = new Food(630, 400, 'cookingPot', this, () => console.log("Girl is pressed"));
-        const cN = new Food(630, 300, 'corn', this, () => console.log("Girl is pressed"));
-        const gO= new Food(630, 500, 'green', this, () => console.log("Girl is pressed"));
-        const bL = new Food(630, 610, 'bowl', this, () => console.log("Girl is pressed"));*/
+        let subTextConfig = {
+            fontFamily: 'Georgia',
+            fontSize: '28px',
+            color: 'white', //#843605
+            align: 'center',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+          }
+
+        this.add.text(400, 100, 'Tap Cooking Pot To Start', subTextConfig).setOrigin(0.5);
+
+        //const cP = new Food(630, 400, 'cookingPot', this, () => console.log("Girl is pressed"));
+        const cN = new Food(630, 300, 'corn', this, () => this.count += 1);
+        const gO= new Food(170, 300, 'green', this, () => this.count += 1);
+        const bL = new Food(400, 500, 'bowl', this, () => this.count += 1);
 
 
 
