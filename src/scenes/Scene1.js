f915c5e1a62e5092e510086071090bd173faa380
tristynl <tristyn-lai@outlook.com>
2023-06-13 21:48:51 -0700

    Had to revise the timer

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -59,9 +59,6 @@ class Scene1 extends Phaser.Scene {
         this.gameOver = false;
     
         this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
-            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-            //this.add.text(game.config.width/2, game.config.height/2, 'High Score: ' + highscore, scoreConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or ← to Menu', scoreConfig).setOrigin(0.5);
             this.gameOver = true;
         }, null, this);
 
