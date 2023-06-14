bbdf16f2d36d2fc594d8b2a9fad241d9344d1883
Samuel Zhang <sashzhan@ucsc.edu>
2023-06-13 18:26:48 -0700

    added dead firefly and updated credits page

--- src/prefabs/Instruction.js
+++ src/prefabs/Instruction.js
@@ -4,7 +4,7 @@ class Instruction extends Phaser.Scene {
     }
 
     create() {
-        const text = this.add.text(100, 180, 'Scene1: Arrowkeys <-> to move \n left and right', {
+        const text = this.add.text(100, 180, 'Created by Tristyn Lai and Samuel Zhang \n music credits from pixabay', {
             fontFamily: 'Courier',
             fontSize: '28px',
             color: '#3c91d6',
@@ -15,27 +15,6 @@ class Instruction extends Phaser.Scene {
             },
         });
 
-        const text1 = this.add.text(50, 240, 'Scene2: Click to obtain fireflies', {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            color: 'pink',
-            align: 'center',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-        });
-
-        const text2 = this.add.text(50, 340, 'Scene3: ', {
-            fontFamily: 'Courier',
-            fontSize: '24px',
-            color: '#3c91d6',
-            align: 'center',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-        });
     }
     
 }
